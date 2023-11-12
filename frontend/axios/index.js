cf5d4f03c5634cdd7b0c25a27cb05d5703c00ddd
Arron Alsop <alsop.arron@gmail.com>
2023-11-12 10:39:02 -0800

    'withAuth'

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,13 @@
+import axios from "axios"
 // ✨ implement axiosWithAuth
+
+export default axiosWithAuth = () => {
+    const token = localStorage.getItem('token')
+    return axios.create({
+        baseURL: 'http://localhost:9000/api',
+        headers: {
+            Authorization: token,
+        },
+    })
+}
+
